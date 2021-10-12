8d8ca398a2f007b71736baad4152a45242a9aa7b
en0rris <g0nefishin34@gmail.com>
2021-10-11 21:10:54 -0400

    A commit because it said so

--- functional_tests.py
+++ functional_tests.py
@@ -40,20 +40,27 @@ class NewVisitorTest(unittest.TestCase):
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
     
-    table=self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elementss_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows)
-      )
-      
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 #there is still a text box inviting her to add another item.
 #she enters "use peacock feather to make a fly"
-    self.fail('Finish the test!')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
 #The page updates again, and now shows both items on her list
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 #Edith wonders whether the site will remember her list. then she 
 #sees that the site has generated a unique URL for her -- there is
 #some explanatory text to that effect
+    self.fail('Finish the test!')
 
 #she visits that url - her to-do list is still there
 
